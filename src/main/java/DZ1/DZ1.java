d3ed45988be6592e1be6e53a183211b4edcdeab5
LavrLitvinov <lux-t@mail.ru>
2021-07-18 20:51:20 +0300

    Дополнено 1 дз

--- src/main/java/DZ1/DZ1.java
+++ src/main/java/DZ1/DZ1.java
@@ -3,14 +3,15 @@ package DZ1;
 public class DZ1 {
     public static void main(String[] args) {
 
-        CanAll[] canalls = {new Cat(1, 1, true),
-                new Human(2,3,true),
-                new Robot(1, 25, true)};
+        CanAll[] canalls = {new Cat(26, 19, true),
+                new Human(8, 19, true),
+                new Robot(16, 17, true)};
 
-        Overskiprunable[] overskiprunables = {  new Treadmill(5),new Barrier(12)};
+        Overskiprunable[] overskiprunables = {new Treadmill(5), new Barrier(12), new Treadmill(17),
+                new Barrier(16), new Barrier(9), new Treadmill(15), new Barrier(29)};
 
         for (CanAll canable : canalls) {
-            for (Overskiprunable overrun : overskiprunables){
+            for (Overskiprunable overrun : overskiprunables) {
                 overrun.overskiprun(canable);
 
             }
